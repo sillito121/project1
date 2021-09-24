2b2b6ee191f40cbf075b0aff873fce2588948b96
sillito121 <sillito121@gmail.com>
2021-09-23 19:36:43 -0600

    Fixed Bugs

--- Project1_Starter_Code/LComAutomaton.cpp
+++ Project1_Starter_Code/LComAutomaton.cpp
@@ -52,9 +52,6 @@ void LComAutomaton::S3(const std::string& input){
         if(input[index+1] == '#'){
             inputRead+=2;
         } else if (input[index+1]!=EOF && input[index+1] != '#'){
-            if(input[index+1]=='\n'){
-                newLines++;
-            }
             index++;
             S3(input);
         }
